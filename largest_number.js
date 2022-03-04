7f97ebfa65120c5bef220eb3d667cc62252f8103
Raisa <9156403+fraisai@users.noreply.github.com>
2022-03-04 16:36:23 -0500

    Create largest_number.js

---
+++ largest_number.js
@@ -0,0 +1,23 @@
+/*
+Challenge: Return Largest Numbers in Arrays
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+*/
+
+function largestOfFour(arr) {
+ let result = [];
+ for (let i in arr) {
+   let largest = arr[i][0];
+   for (let j = 1; j < arr[i].length; j++) {
+     if (arr[i][j] > largest) {
+       largest = arr[i][j]
+     }
+   }
+   result.push(largest)
+ }
+ return result;
+}
+
+//largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]);
