e31869253eff49ca80d0a85ce93156b58143451f
Raisa <9156403+fraisai@users.noreply.github.com>
2022-03-14 13:35:34 -0400

    Update arrToObj.js

--- arrToObj.js
+++ arrToObj.js
@@ -1,5 +1,6 @@
 /*
-Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values.
+Add code to the function arrToObj in the place marked "ADD CODE HERE" in order to achieve the desired console logs. arrToObj should return an object 
+that has elements from the passed-in array as keys, and the outputs from the callback (when those elements are passed in) as the corresponding values.
 
 */
 
