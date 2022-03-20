92428e2fa028958fc8ca8f0f032ed862b385be72
Raisa <9156403+fraisai@users.noreply.github.com>
2022-03-20 14:47:39 -0400

    Create dateStamp.js

---
+++ dateStamp.js
@@ -0,0 +1,22 @@
+/*
+Create a function dateStamp that accepts a function and returns a function. 
+The returned function will accept whatever arguments the passed-in function accepts and return an object with a date key whose value is 
+today's date (not including the time) represented as a human-readable string (see the Date object for conversion methods), 
+and an output key that contains the result from invoking the passed-in function.
+*/
+
+function dateStamp(func) {
+  let obj = {};
+  
+  function second_func(...args) {
+    let date = new Date();
+    obj["date"] = date.toDateString();
+    obj["output"] = func(...args)
+    return obj;
+  }
+  return second_func;
+}
+// Uncomment these to check your work!
+const stampedMultBy2 = dateStamp(n => n * 2);
+console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
