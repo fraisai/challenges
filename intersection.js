0c7735a41d0938e1b7dd1f5006986a9ece6b351c
Raisa <9156403+fraisai@users.noreply.github.com>
2022-03-21 18:10:56 -0400

    Update intersection.js

--- intersection.js
+++ intersection.js
@@ -3,6 +3,26 @@ Construct a function intersection that compares input arrays and returns a new a
 
 */
 
+// Method 1: Using .reduce and filter
+function intersection(arrays) {
+  let result = arrays.reduce((prev, curr) => {  
+    return prev.filter(element => {
+      console.log(element)
+      return curr.includes(element)
+    });
+  });
+  return result;
+}
+// Uncomment these to check your work!
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+const arr4 = [0, 0,  0];
+
+console.log(intersection([arr1, arr2, arr3, arr4])); // should log: [5, 15]
+
+
+// Method 2: Using .forEach and check method? How did I even come up with this?
 function intersection(arrays) {
   const result = [];
   let first_array = arrays[0]
@@ -33,7 +53,7 @@ let arr6 = [1, 2, 3, 4]
 console.log(intersection([arr5, arr6, [8, 8, 4], [1, 2, 3, 4, 5, 6, 7], [2, 3]]));
 
 
-// METHOD 2 (Using .reduce() method)
+// METHOD 3: (Using .reduce() method)
 function intersection2(arrays) {
   let arr = [];
   // flatten arrays into 1 single array
