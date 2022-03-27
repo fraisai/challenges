c13ca702da64a5b9191a4c5956ba73b45f38670a
Raisa <9156403+fraisai@users.noreply.github.com>
2022-03-27 13:58:26 -0400

    Update defineFirstArg.js

--- defineFirstArg.js
+++ defineFirstArg.js
@@ -16,3 +16,25 @@ function defineFirstArg(func, ...args) {
 const subtract = function(big, small) { return big - small; };
 const subFrom20 = defineFirstArg(subtract, 20);
 console.log(subFrom20(5)); // should log: 15
+
+
+// METHOD 2:
+// input: function and an argument (passed in function accepts 1 argument or more)
+// output: function
+
+// inner function: invokes the passed in function with the passed in argument as the passed in function's 1st argument and additional arguments needed by the passed-in function will need to be passed into the returned function
+
+function defineFirstArg2(func, arg) {
+  console.log(arg)
+  function perf(...arg1) {
+    console.log(arg1)
+    let answer = func(arg, ...arg1);
+    return answer;
+  }
+  return perf;
+}
+
+// Uncomment these to check your work!
+const subtract = function(big, small) { return big - small; };
+const subFrom20 = defineFirstArg2(subtract, 20);
+console.log(subFrom20(5)); // should log: 15
