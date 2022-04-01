6986cf3a46a21b0b52a6fdd13b7f2680e6254103
Raisa <9156403+fraisai@users.noreply.github.com>
2022-04-01 12:51:47 -0400

    Update recursion_countdown.js

--- recursion_countdown.js
+++ recursion_countdown.js
@@ -1,5 +1,6 @@
 // Challenge: Use recursion to return an array containing the countdown numbers
 
+// METHOD 1
 function countdown(n){
  let result = [];
  if (n < 1) {
@@ -10,4 +11,18 @@ function countdown(n){
  return result;
 }
 
-countdown(10)
+countdown(10);
+
+// METHOD 2: Challenge 1
+// function countdown(n) {
+//   if (n === 1){
+//     console.log(n);
+//     return 1;
+//   }
+//   console.log(n);
+//   return countdown(n-1);
+// }
+
+// // To check if you've completed it, uncomment these console.logs!
+// countdown(5);
+// countdown(10);
