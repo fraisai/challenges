f7519ac20627b909cefdf86236fd735cf29949d4
Raisa <9156403+fraisai@users.noreply.github.com>
2022-04-11 16:48:33 -0400

    Create truncateString.js

---
+++ truncateString.js
@@ -0,0 +1,14 @@
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+function truncateString(str, num) {
+ let newStr = "";
+ if (str.length > num) {
+   newStr = str.substr(0, num)
+ } else {
+   return str;
+ }
+ return newStr + "...";
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
