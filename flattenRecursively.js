fd22cb1c1e58dbcfcbd2422acede8374c939b90a
Raisa <9156403+fraisai@users.noreply.github.com>
2022-05-03 18:26:23 -0400

    Create flattenRecursively.js

---
+++ flattenRecursively.js
@@ -0,0 +1,38 @@
+/*
+Write a recursive function flattenRecursively that flattens a nested array. Your function should be able to handle varying levels of nesting.
+*/
+
+function flattenRecursively(arr, result = [], round = 0) {
+  console.log("arr at the beginning: ", arr)
+  console.log("result at beginning:", result)
+
+  if (arr.length === 0) {
+    console.log("base case:")
+    return result
+  }
+  
+  // if the 1st element of arr is an array
+  if (arr[0].constructor === Array) {
+    console.log("\nif - round:", ++round)
+
+    let newArr = arr[0].concat(arr.slice(1));
+    console.log("newArr", newArr);
+    
+    return flattenRecursively(newArr, result, round);
+    
+  } else {
+    console.log("else - round", ++round)
+    result.push(arr[0]);
+    console.log("result in arr ", arr)
+  }
+  
+  console.log("final result", result)
+
+  return flattenRecursively(arr.slice(1), result, round);
+}
+
+// console.log(flattenRecursively([ 1, [ 2, 3, [ 4 ] ] ])); //-> [1, 2, 3, 4]
+// console.log(flattenRecursively([[1], {}, [3, [[4]]]])); //-> [1, {}, 3, 4]
+console.log(flattenRecursively([[[1]], [[[[[8]]]]], {}, [3, [[4, 9]]]])); //-> [1, {}, 3, 4]
+let test = [[[1]]]
+console.log("test", test[0][0][0])
