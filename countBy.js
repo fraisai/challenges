5a1c20df2af39fc7e403e640939e47ce9ffb6be3
Raisa <9156403+fraisai@users.noreply.github.com>
2022-05-18 12:41:13 -0400

    Update countBy.js

--- countBy.js
+++ countBy.js
@@ -9,33 +9,29 @@ The value associated with each key will be the number of times that particular r
 function countBy(array, callback) {
   let obj = {};
   let result = [];
-  let count = 0;
+  let count = 0; // keeps track of the number of times value appears
+  
+  // Store the outputs of the elements of array passed in as an argument into the callback function in result array.
   for (let i in array) {
     result.push(callback(array[i]))
   }
-  for (let i in result) {
-    if (Object.keys(obj).includes(result[i])) {
+  
+  result.forEach(el => {
+    if (Object.keys(obj).includes(el) {  // if an element of result is a key in obj
       count += 1;
-      obj[result[i]] = count;
+      obj[el] = count;
     } else {
       count = 0;
-      obj[result[i]] = count;
+      obj[el] = count;
     }
+  });
 
-  }
-  console.log(result)
-  
-  
-  //iterate through array and perform callback(element)
-  // object[return from callback] = number of times value was returned
-  
-  //returns object
   return obj;
 }
 // Uncomment these to check your work!
-function evenOdd(n) {
-  if (n % 2 === 0) return 'even';
-  else return 'odd';
-}
-const nums = [1, 2, 3, 4, 5];
-console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+// function evenOdd(n) {
+//   if (n % 2 === 0) return 'even';
+//   else return 'odd';
+// }
+// const nums = [1, 2, 3, 4, 5];
+// console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
