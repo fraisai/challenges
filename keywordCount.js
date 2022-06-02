bf2ff109987db3946995ddeddfd1454133b55bf4
Raisa <9156403+fraisai@users.noreply.github.com>
2022-06-02 10:42:56 -0400

    Create keywordCount.js

---
+++ keywordCount.js
@@ -0,0 +1,19 @@
+/*
+
+Given an arbitrarily nested array of strings, and a target keyword strong,
+return the number of times a keyword appears in a nested array of arrays.
+
+keywordCount(['bye', 'hi', ['cool', 'hi']], 'hi') -> 2 because 'hi' appears twice
+keywordCount(['x', 'y', ['x', 'x'], 'a'], 'x') -> 3
+keywordCount(['blah', 'key', ['inside', ['really inside']]], 'lol') -> 0
+
+*/
+
+const keywordCount = (array, keyword) => {
+    let newArr = array.flat(Infinity);
+    let count = 0;
+    newArr.forEach(element => {
+        if (element === keyword) count += 1
+    });
+    return count;
+};
