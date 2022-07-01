9c3e4bdecbb450813c44d54391022a6fec48f44a
Raisa <9156403+fraisai@users.noreply.github.com>
2022-07-01 17:21:25 -0400

    Update makePersonObject.js

--- makePersonObject.js
+++ makePersonObject.js
@@ -1,15 +1,24 @@
 /*
-Create a function that has two parameters (name and age) and returns an object. Let's call this function makePerson. This function will:
-Create an empty object
-Add a name property to the newly created object with its value being the 'name' argument passed into the function
-Add an age property to the newly created object with its value being the 'age' argument passed into the function
+Create a function that has two parameters (name and age) and returns an object. Let's call this function makePerson. 
+
+This function will:
+1. Create an empty object
+2. Add a name property to the newly created object with its value being the 'name' argument passed into the function
+3. Add an age property to the newly created object with its value being the 'age' argument passed into the function
+4. Create a property greet where the value is a function that logs "hello".
+
 Return the object
+
+
 */
 
 function makePersonObject(name, age) {
 	const obj = {
-    name: name,
-    age: age
+		name: name,
+		age: age, 
+		greet: (name) => {
+			console.log(`Hello ${name}`);
+		}
   };
   console.log(obj)
   return obj;
@@ -21,3 +30,4 @@ const vicky = makePersonObject('Vicky', 24);
 // /********* Uncomment these lines to test your work! *********/
 console.log(vicky.name); // -> Logs 'Vicky'
 console.log(vicky.age); // -> Logs 24
+console.log(vicky.greet()); // -> Logs 'Hello Vicky'
